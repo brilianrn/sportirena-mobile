f9d535e50296b4534b6ecfac1c8dc671a0e921ef
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2023-08-27 21:54:16 +0700

    fix venue & vennue detail

--- App.tsx
+++ App.tsx
@@ -1,38 +1,57 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {
-  HomeScreen,
-  LoginScreen,
-  RegisterScreen,
-  ResetPasswordScreen,
-} from "./src/screens";
+import React from "react";
+import { Provider } from "react-redux";
 import {
   homeName,
   loginName,
   registerName,
   resetPasswordName,
+  venueDetailName,
+  venueName,
 } from "./src/constants";
+import {
+  HomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  VenueDetailScreen,
+  VenueScreen,
+} from "./src/screens";
+import store from "./src/store";
+import { ToastProvider } from "react-native-toast-notifications";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={homeName}
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name={loginName} component={LoginScreen} />
-        <Stack.Screen name={registerName} component={RegisterScreen} />
-        <Stack.Screen
-          name={resetPasswordName}
-          component={ResetPasswordScreen}
-        />
-        <Stack.Screen name={homeName} component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <React.StrictMode>
+      <ToastProvider>
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName={homeName}
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Stack.Screen name={loginName} component={LoginScreen} />
+              <Stack.Screen name={registerName} component={RegisterScreen} />
+              <Stack.Screen
+                name={resetPasswordName}
+                component={ResetPasswordScreen}
+              />
+              <Stack.Screen name={homeName} component={HomeScreen} />
+              <Stack.Screen name={venueName} component={VenueScreen} />
+              <Stack.Screen
+                name={venueDetailName}
+                component={VenueDetailScreen}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+      </ToastProvider>
+    </React.StrictMode>
   );
 };
 
--- App.type.ts
+++ App.type.ts
@@ -2,3 +2,7 @@ export type OptionType = {
   value: string | number;
   label: string;
 };
+
+export type ToastType = "normal" | "success" | "warning" | "danger" | "custom";
+
+export type ToastPosition = "top" | "bottom";
